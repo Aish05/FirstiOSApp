54fb9bd671b441433a30732484540c3027e60f5e
Aish05 <rkaishwarya5@gmail.com>
2020-05-18 16:45:17 +0530

    My first iOS app

---
+++ I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Aishwarya, Radhakrishnan Iyer on 18/05/20.
+//  Copyright © 2020 Aishwarya, Radhakrishnan Iyer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
